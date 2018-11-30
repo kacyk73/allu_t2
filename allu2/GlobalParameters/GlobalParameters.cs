87635a5ecacad9e6304270a93494d2d8f2c54b35
kacyk73 <pk@koczyk.pl>
2018-11-30 20:43:20 +0100

    mouse position comparator implemented

--- allu2/GlobalParameters/GlobalParameters.cs
+++ allu2/GlobalParameters/GlobalParameters.cs
@@ -5,8 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace allu2
-{
-
+{ 
     //terrain type
     public enum TerrainKind
     {
@@ -30,6 +29,9 @@ namespace allu2
         public int boardBoxDim = 20;
         public int boardDimX = 60;
         public int boardDimY = 40;
-        
+
+        //out of range parameter
+        public const int OutOfRange = 999;
+
     }
 }
---
+++ allu2/Mouse/PosXY.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace allu2
+{
+    //the map position class
+    public class PosXY : IEquatable<PosXY>
+    {
+        public PosXY()
+        {
+            PosX = GlobalParameters.OutOfRange;
+            PosY = GlobalParameters.OutOfRange;
+            
+        }
+        public int PosX { get; set; }
+        public int PosY { get; set; }
+
+        // override object.Equals
+        public bool Equals(PosXY other)
+        {
+            return this.PosX == other.PosX && this.PosY == other.PosY;
+            //return base.Equals(obj);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is PosXY)
+                return Equals((PosXY)obj);
+            else
+                return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return PosX.GetHashCode() ^ PosY.GetHashCode();
+        }
+    }
+}
